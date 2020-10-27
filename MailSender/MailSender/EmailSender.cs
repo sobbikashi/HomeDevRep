30a58f89afb740d9a643e43992f9df2fc52893ed
SobbikashiGit <xt207@yandex.ru>
2020-10-27 19:37:20 +0300

    initial

---
+++ MailSender/MailSender/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MailSender.Model
+{
+    class EmailSender
+    {
+        //string addressFrom = new 
+        public void Send(MailMessage message, string password)
+        {
+            try
+            {
+                string subject = message.Subject;
+                string body = message.Body;
+                var smtp = new SmtpClient()
+                {
+                    
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    DeliveryFormat = SmtpDeliveryFormat.International,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("test.send207@gmail.com", password)
+                };
+                smtp.Send("test.send207@gmail.com", "test.get207@yandex.ru", " ", "Это сообщение отправлено с помощью слепленной на коленке программы");
+                Debug.WriteLine("Message has sent");
+                MessageBox.Show("The Message has beed sent");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ MailSender/MailSender/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using System.Net.Mail;
+using MailSender.Model;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+             
+
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Model.EmailSender emailSender = new Model.EmailSender();
+            MailMessage mail = new MailMessage();
+            //MailAddress from = new MailAddress(tFrom.Text);
+            //MailAddress to = new MailAddress(tTo.Text);
+            emailSender.Send(mail, tPassword.Password);
+        }
+    }
+}
