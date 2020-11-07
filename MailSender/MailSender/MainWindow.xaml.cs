90dd4ff8f8e0d35484f6c72996eff95724e1318c
SobbikashiGit <xt207@yandex.ru>
2020-11-07 15:35:56 +0300

    добавлены кнопки с нерабочими картинками #неработает

--- MailSender/MailSender/MainWindow.xaml.cs
+++ MailSender/MailSender/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Net.Mail;
 using MailSender.Model;
-using System.Text.RegularExpressions;
-using System.Media;
+using System.Media;		
+
 
 namespace MailSender
 {
@@ -90,13 +79,21 @@ namespace MailSender
         }
 
         private void btnNext_Click(object sender, RoutedEventArgs e)
-        {
-            this.tcMain.SelectedIndex = 1;
+        { 
+            if (this.tcMain.SelectedIndex != 3)
+            {
+                this.tcMain.SelectedIndex += 1;
+            }               
+            else this.tcMain.SelectedIndex = 0;
         }
 
         private void btnPrev_Click(object sender, RoutedEventArgs e)
         {
-            this.tcMain.SelectedIndex = -1;
+            if (this.tcMain.SelectedIndex != 0)
+            {
+                this.tcMain.SelectedIndex -= 1;
+            }               
+            else this.tcMain.SelectedIndex = 3;
         }
     }
 }
